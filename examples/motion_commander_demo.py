2d0e95ee356c735613607eb321a2bb04030cacb6
Tom van Dijk <tomvand@users.noreply.github.com>
2018-06-20 15:42:16 +0200

    Small changes to examples

--- examples/motion_commander_demo.py
+++ examples/motion_commander_demo.py
@@ -40,7 +40,7 @@ from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 
-URI = 'radio://0/70/2M'
+URI = 'radio://0/80/250K'
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
